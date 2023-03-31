dd971b18f156ec5356b3560f781ac216f1e6335b
GustavoSoatto <gustavo.soatto@hotmail.com>
2023-03-30 22:40:16 -0300

    1.6
    Ta bão

--- Part_II.cpp
+++ Part_II.cpp
@@ -16,12 +16,13 @@
 // vii. Sair
 
 #include <iostream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
 struct parcela
 {
-    int valor;
+    float valor;
     bool pagamento;
     string data_pagamento;
     string data_vencimento;
@@ -34,7 +35,8 @@ struct cliente
     string endereco;
     string data_nascimento;
     string data_emprestimo;
-    string total_emprestimo;
+    float total_emprestimo;
+    float total_parcelas;
     parcela parcelas_mensais[12];
 };
 
@@ -104,7 +106,7 @@ int meses(string mes_string)
     }
     else
     {
-        cout << "Mês inválido\n";
+        cout << "Mes invalido\n";
         return 0;
     }
 }
@@ -122,7 +124,7 @@ string meses_for_string(int mes_number)
         mes_string = "Fevereiro";
         return mes_string;
     case 3:
-        mes_string = "Março";
+        mes_string = "Marco";
         return mes_string;
     case 4:
         mes_string = "Abril";
@@ -152,13 +154,14 @@ string meses_for_string(int mes_number)
         mes_string = "Dezembro";
         return mes_string;
     default:
-        cout << "Mês inválido, informe um número entre 1 - 12 de acordo com o mês desejado" << endl;
+        cout << "Mes invalido, informe um número entre 1 - 12 de acordo com o mes desejado" << endl;
         return 0;
     }
 }
 
 int main()
 {
+    setlocale(LC_ALL, "pt_br.UTF-8");
     int user_option = 0, user_cliente_quantidade = 0, user_option_2 = 0;
 
     cout << "Quantos cliente deseja cadastrar?\n";
@@ -168,10 +171,10 @@ int main()
     do
     {
         cout << "1. Cadastrar cliente\n"
-             << "2. Visualizar as informações de um cliente\n"
-             << "3. Editar as informações de um cliente\n"
+             << "2. Visualizar as informacoes de um cliente\n"
+             << "3. Editar as informacoes de um cliente\n"
              << "4. Cadastrar parcela paga\n"
-             << "5. Cadastrar empréstimo\n"
+             << "5. Cadastrar emprestimo\n"
              << "6. Excluir cliente\n"
              << "7. SAIR\n";
         cin >> user_option;
@@ -185,14 +188,14 @@ int main()
                 cin >> user_cliente[i].nome;
                 cout << "CPF\n";
                 cin >> user_cliente[i].cpf;
-                cout << "Endereço\n";
+                cout << "Endereco\n";
                 cin >> user_cliente[i].endereco;
                 cout << "Data de nascimento\n";
                 cin >> user_cliente[i].data_nascimento;
-                cout << "Data do empréstimo\n";
-                cin >> user_cliente[i].data_emprestimo;
-                cout << "Valor total do empréstimo\n";
-                cin >> user_cliente[i].total_emprestimo;
+
+                user_cliente[i].data_emprestimo = "00/00/00";
+                user_cliente[i].total_emprestimo = 0.0;
+                user_cliente[i].total_parcelas = 0.0;
                 for (int j = 0; j < 12; j++)
                 {
                     user_cliente[i].parcelas_mensais[j].valor = 0000;
@@ -214,11 +217,12 @@ int main()
                      << user_cliente[user_option - 1].endereco << endl
                      << user_cliente[user_option - 1].data_nascimento << endl
                      << user_cliente[user_option - 1].data_emprestimo << endl
-                     << user_cliente[user_option - 1].total_emprestimo << endl;
+                     << user_cliente[user_option - 1].total_emprestimo << endl
+                     << user_cliente[user_option - 1].total_parcelas << endl;
                 for (int k = 0; k < 12; k++)
                 {
-                    cout << "Informações das parcelas do mês de " << meses_for_string(k + 1) << endl;
-                    cout << user_cliente[user_option - 1].parcelas_mensais[k].valor << endl;
+                    cout << "Informacoes das parcelas do mes de " << meses_for_string(k + 1) << endl;
+                    cout << setprecision(2) << user_cliente[user_option - 1].parcelas_mensais[k].valor << endl;
                     cout << user_cliente[user_option - 1].parcelas_mensais[k].pagamento << endl;
                     cout << user_cliente[user_option - 1].parcelas_mensais[k].data_pagamento << endl;
                     cout << user_cliente[user_option - 1].parcelas_mensais[k].data_vencimento << endl;
@@ -226,7 +230,7 @@ int main()
             }
             else
             {
-                cout << "Opção inválida, você tem " << user_cliente_quantidade << " cliente(s).\n";
+                cout << "Opcao invalida, você tem " << user_cliente_quantidade << " cliente(s).\n";
             }
         }
         else if (user_option == 3) // iii. Editar as informações de um cliente
@@ -237,16 +241,15 @@ int main()
                 string mes = "";
                 char pagamento = '\n';
 
-                cout << "Qual cliente deseja fazer alterações? (número do cliente)\n";
+                cout << "Qual cliente deseja fazer alteracoes? (numero do cliente)\n";
                 cin >> user_option;
-                cout << "Qual dado deseja fazer a alteração?\n"
+                cout << "Qual dado deseja fazer a alteracao?\n"
                      << "1. Nome do cliente\n"
                      << "2. CPF\n"
                      << "3. Endereço\n"
                      << "4. Data de nascimento\n"
-                     << "5. Data do empréstimo\n"
-                     << "6. Valor total do empréstimo\n"
-                     << "7. Informações sobre parcelas mensais de empréstimo\n"
+                     << "5. Informacoes das parcelas\n"
+                     << "6. Informacoes dos emprestimos\n"
                      << "0. CANCELAR" << endl;
                 cin >> user_option_2;
                 if (user_option_2 == 1)
@@ -267,19 +270,11 @@ int main()
                 }
                 else if (user_option_2 == 5)
                 {
-                    cin >> user_cliente[user_option - 1].data_emprestimo;
-                }
-                else if (user_option_2 == 6)
-                {
-                    cin >> user_cliente[user_option - 1].total_emprestimo;
-                }
-                else if (user_option_2 == 7)
-                {
                     do
                     {
-                        cout << "Qual mês deseja fazer a alteração? (jan,fev,mar,...)\n";
+                        cout << "Qual mês deseja fazer a alteracao? (jan,fev,mar,...)\n";
                         cin >> mes;
-                        cout << "Qual dado da parcela deseja fazer a alteração?\n"
+                        cout << "Qual dado da parcela deseja fazer a alteracao?\n"
                              << "1. Valor\n"
                              << "2. Pagamento(s/n)\n"
                              << "3. Data do pagamento\n"
@@ -289,7 +284,7 @@ int main()
                         switch (user_option_3)
                         {
                         case 1:
-                            cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes)].valor;
+                            user_cliente[user_option - 1].parcelas_mensais[meses(mes)].valor = (user_cliente[user_option - 1].total_emprestimo / user_cliente[user_option - 1].total_parcelas);
                             break;
                         case 2:
                             cin >> pagamento;
@@ -303,7 +298,7 @@ int main()
                             }
                             else
                             {
-                                cout << "Pagamento inválido\n";
+                                cout << "Pagamento invalido\n";
                             }
                             break;
                         case 3:
@@ -317,6 +312,40 @@ int main()
                         }
                     } while (user_option_3 != 0);
                 }
+                else if (user_option_2 == 6)
+                {
+                    do
+                    {
+                        cout << "Qual dado do emprestimo deseja fazer a alteracao?\n"
+                             << "1. Data do emprestimo\n"
+                             << "2. Valor do emprestimo\n"
+                             << "3. Quantidade de parcelas que deseja pagar\n"
+                             << "0. CANCELAR" << endl;
+                        cin >> user_option_3;
+                        switch (user_option_3)
+                        {
+                        case 1:
+                            cin >> user_cliente[user_option - 1].data_emprestimo;
+                            break;
+                        case 2:
+                            cin >> user_cliente[user_option - 1].total_emprestimo;
+                            break;
+                        case 3:
+                            cin >> user_cliente[user_option - 1].total_parcelas;
+                            break;
+                        }
+                    } while (user_option_3 != 0);
+
+                    cin >> user_cliente[user_option - 1].data_emprestimo;
+                }
+                else if (user_option_2 == 6)
+                {
+                    cin >> user_cliente[user_option - 1].total_emprestimo;
+                }
+                else if (user_option_2 == 8)
+                {
+                    cin >> user_cliente[user_option - 1].total_parcelas;
+                }
             } while (user_option_2 != 0);
         }
         else if (user_option == 4) // iv. Cadastrar parcela paga
@@ -325,33 +354,51 @@ int main()
             char pagamento = '\0';
             string mes = "";
 
-            cout << "Qual cliente deseja fazer alterações? (número do cliente)\n";
+            cout << "Qual cliente deseja cadastrar a parcela? (numero do cliente)\n";
             cin >> user_option;
-            cout << "Qual mês deseja cadastrar?(jan,fev,mar,...)\n";
-            cin >> mes;
-            cout << "Qual o valor da parcela?\n";
-            cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].valor;
-            cout << "A parcela já foi paga?(s/n)\n";
-            cin >> pagamento;
-            if (pagamento == 's')
+
+            if (user_cliente[user_option - 1].total_emprestimo > 0)
             {
-                user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].pagamento = true;
-                cout << "Qual a data do pagamento?(00/00/00)\n";
-                cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].data_pagamento;
-            }
-            else if (pagamento == 'n')
-            {
-                user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].pagamento = false;
+                cout << "Qual mes deseja cadastrar?(jan,fev,mar,...)\n";
+                cin >> mes;
+                user_cliente[user_option - 1].parcelas_mensais[meses(mes)].valor = user_cliente[user_option - 1].total_emprestimo / user_cliente[user_option - 1].total_parcelas;
+                cout << "O valor e: \n"
+                     << user_cliente[user_option - 1].parcelas_mensais[meses(mes)].valor << endl;
+                cout << "A parcela ja foi paga?(s/n)\n";
+                cin >> pagamento;
+                if (pagamento == 's')
+                {
+                    user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].pagamento = true;
+                    cout << "Qual a data do pagamento?(00/00/00)\n";
+                    cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].data_pagamento;
+                }
+                else if (pagamento == 'n')
+                {
+                    user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].pagamento = false;
+                }
+                else
+                {
+                    cout << "Pagamento invalido\n";
+                }
+                cout << "Qual a data do vencimento?(00/00/00)\n";
+                cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].data_vencimento;
             }
             else
             {
-                cout << "Pagamento inválido\n";
+                cout << "O cliente deve ter um emprestimo ativo para poder pagar uma parcela.\n";
             }
-            cout << "Qual a data do vencimento?(00/00/00)\n";
-            cin >> user_cliente[user_option - 1].parcelas_mensais[meses(mes) - 1].data_vencimento;
         }
         else if (user_option == 5) // v. Cadastrar empréstimo.
         {
+            cout << "Qual cliente deseja cadastrar um emprestimo?\n";
+            cin >> user_option;
+
+            cout << "Qual o valor do emprestimo?(Use casa decimais 0.00)\n";
+            cin >> user_cliente[user_option - 1].total_emprestimo;
+            cout << "Quantas parcelas deseja pagar? (máximo de 12 parcelas)\n";
+            cin >> user_cliente[user_option - 1].total_parcelas;
+            cout << "Qual a data do emprestimo?(00/00/00)\n";
+            cin >> user_cliente[user_option - 1].data_emprestimo;
         }
         else if (user_option == 6) // vi. Excluir cliente
         {
@@ -367,7 +414,7 @@ int main()
             }
             else
             {
-                cout << "Opção inválida, você tem " << user_cliente_quantidade << " cliente(s).\n";
+                cout << "Opcao invalida, você tem " << user_cliente_quantidade << " cliente(s).\n";
             }
         }
         else if (user_option == 7) // vii. Sair
@@ -376,7 +423,7 @@ int main()
         }
         else
         {
-            cout << "Opção inválida (use apenas números 1.2.3.4.5.6.7)\n";
+            cout << "Opcao invalida (use apenas numeros 1.2.3.4.5.6.7)\n";
         }
     } while (user_option != 7);
 }
